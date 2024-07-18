38804aceb4fdd2289c34a83f35afb1f29df5daeb
Mike <tinhminhvt246@gmail.com>
2024-07-18 22:06:11 +0700

    add

---
+++ Assets/Char_Move.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
+
+public class Char_Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Rigidbody2D myRigidBody2D;
+    public float MovementSpeed = 0;
+    public int Health;
+    public int Damage;
+    public int Def;
+    public int Crit;
+
+    public int AttackSpeed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            myRigidBody2D.position = new Vector3(myRigidBody2D.position.x, myRigidBody2D.position.y + MovementSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            myRigidBody2D.position = new Vector3(myRigidBody2D.position.x, myRigidBody2D.position.y - MovementSpeed);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            myRigidBody2D.position = new Vector3(myRigidBody2D.position.x - MovementSpeed, myRigidBody2D.position.y);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            myRigidBody2D.position = new Vector3(myRigidBody2D.position.x + MovementSpeed, myRigidBody2D.position.y);
+        }
+    }
+}
+
